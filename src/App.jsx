8466efa4e717dfdb8b824877c1c5aa84ed4f36b0
joshenx <e0525998@u.nus.edu>
2023-06-27 22:27:04 +0800

    Add more coughs :)

--- src/App.jsx
+++ src/App.jsx
@@ -20,19 +20,43 @@ export default function App({ ready, clicked }) {
   let coughValues = [
     {
       src: "sounds/coughquick.mp3",
-      weight: 0.3,
+      weight: 0.1,
     },
     {
       src: "sounds/coughrare.mp3",
       weight: 0.05,
     },
     {
-      src: "sounds/cough.mp3",
-      weight: 0.3,
+      src: "sounds/cough1.mp3",
+      weight: 0.1,
+    },
+    {
+      src: "sounds/cough2.mp3",
+      weight: 0.1,
     },
     {
       src: "sounds/ringtone.mp3",
-      weight: 0.01,
+      weight: 0.001,
+    },
+    {
+      src: "sounds/femalecough1.mp3",
+      weight: 0.1,
+    },
+    {
+      src: "sounds/femalecough2.mp3",
+      weight: 0.1,
+    },
+    {
+      src: "sounds/femalecough3.mp3",
+      weight: 0.1,
+    },
+    {
+      src: "sounds/femalecoughquick.mp3",
+      weight: 0.1,
+    },
+    {
+      src: "sounds/femalecoughrare.mp3",
+      weight: 0.1,
     },
   ];
 
