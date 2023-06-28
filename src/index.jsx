e190f54410a31cdc81beba77200ba32ad95fc7ec
joshenx <e0525998@u.nus.edu>
2023-06-29 07:27:09 +0800

    Enable Vercel analytics

--- src/index.jsx
+++ src/index.jsx
@@ -1,32 +1,34 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import Intro from './Intro'
-import { ChakraProvider } from '@chakra-ui/react'
-import chakraTheme from '@chakra-ui/theme'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Intro from "./Intro";
+import { ChakraProvider } from "@chakra-ui/react";
+import chakraTheme from "@chakra-ui/theme";
+import { Analytics } from "@vercel/analytics/react";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
 
 const theme = {
-    styles: {
-      global: {
-        'html, body': {
-          color: 'gray.600',
-          lineHeight: 'tall',
-        },
-        a: {
-          color: 'teal.500',
-        },
+  styles: {
+    global: {
+      "html, body": {
+        color: "gray.600",
+        lineHeight: "tall",
+      },
+      a: {
+        color: "teal.500",
       },
     },
-  }
+  },
+};
 
 root.render(
-    <>
-        <ChakraProvider theme={theme}>
-        <Intro>
-        <App/>
-        </Intro>
-        </ChakraProvider>
-    </>
-)
+  <>
+    <Analytics />
+    <ChakraProvider theme={theme}>
+      <Intro>
+        <App />
+      </Intro>
+    </ChakraProvider>
+  </>
+);
